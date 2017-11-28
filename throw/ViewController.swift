d4da7786a9b5d7d7db6600d4799826ae96396cf8
hbc <bcxxxxxx@gmail.com>
2017-11-28 19:04:20 +0800

    Initial Commit

---
+++ throw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  throw
+//
+//  Created by hbc on 28/11/2017.
+//  Copyright © 2017 rocks.hbc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
