6657858dbf297e3d90ba5a70bc505a071a1117b2
hbc <bcxxxxxx@gmail.com>
2017-11-29 00:41:10 +0800

    feat(view): setup views

---
+++ throw/ComposeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ComposeViewController.swift
+//  throw
+//
+//  Created by hbc on 29/11/2017.
+//  Copyright © 2017 b4fun. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+
+    func setupView() {
+        title = "Compose"
+    }
+
+}
---
+++ throw/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  throw
+//
+//  Created by hbc on 28/11/2017.
+//  Copyright © 2017 b4fun. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+
+    func setupView() {
+        title = "Throw!"
+    }
+
+}
+
